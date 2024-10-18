502752cc88e270bce67b1e4c11b52697fd06650a
JorgeMateus56 <jorge.mateus@gmail.com>
2024-10-18 14:52:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import tensorflow as tf
 import io
 from PIL import Image
 import numpy as np
+import pandas as pd
+import plotly.express as px
 
 @st.cache_resource
 #---------------------------------------------------------------------------------------
 def carrega_modelo():
+  
   #https://drive.google.com/file/d/1VPpw18cagLfP8j6smglsCrarV0182Jkh/view?usp=drive_link
   url = 'https://drive.google.com/uc?id=1VPpw18cagLfP8j6smglsCrarV0182Jkh'
   
@@ -18,8 +21,11 @@ def carrega_modelo():
   return interpreter
 #---------------------------------------------------------------------------------------
 def carrega_imagem():
+  
   uploaded_file = st.file_uploader('Arraste e solte uma imagem aqui ou clique para selecionar uma', type=['png','jpg','jpeg'])
+  
   if uploaded_file is not None:
+    
     image_data = uploaded_file.read()
     image = Image.open(io.BytesIO(image_data))
 
@@ -30,9 +36,30 @@ def carrega_imagem():
     image = image / 255.0
     image = np.expand_dims(image,axis=0)
     
-    return image    
+    return image   
+#---------------------------------------------------------------------------------------
+def previsao(interpreter,image):
+  
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+
+  interpreter.set_tensor(input_details[0]['index'],image)
+
+  interpreter.invoke()
+
+  output_data = interpreter.get_tensor(output_details[0]['index'])
+  classes = ['BlackMeasles', 'BlackRot', 'HealthyGrapes', 'LeafBlight']
+
+  df = pd.DataFrame()
+  df['classes'] = classes
+  df['probabilidades (%)'] = 100 * output_data[0]
+
+  fig = px.bar(df,y='classes',x='probabilidades (%)', orientation='h', text='probabilidades (%)',
+        title='Probabilidade de Classes de Doencas em Uvas')
+  st.plotly.chart(fig)
 #---------------------------------------------------------------------------------------
 def main():
+  
   st.set_page_config(
     page_title="Classifica Folhas de Videira",
     page_icon=" "
@@ -47,6 +74,8 @@ def main():
   image = carrega_imagem()
 
   # Classifica
+  if image is not None:
+    previsao(interpreter,image)
 
 #--------------------------------------------------------------------------------------
 
